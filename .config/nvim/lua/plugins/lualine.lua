9ed0c6d845e915895542babd311d14d9f99ba8c3
Matteo <matteo@Matteos-MacBook-Pro.local>
2026-03-14 18:12:27 +0100

    Add lualine

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,24 @@
+return {
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("lualine").setup({
+                options = {
+                    theme = "vscode", -- Automatically matches your theme
+                    component_separators = { left = "|", right = "|" },
+                    section_separators = { left = "", right = "" },
+                    globalstatus = true, -- One statusline at the bottom for all windows
+                },
+                sections = {
+                    lualine_a = { "mode" },
+                    lualine_b = { "branch", "diff", "diagnostics" },
+                    lualine_c = { { "filename", path = 1 } }, -- Shows relative path
+                    lualine_x = { "encoding", "fileformat", "filetype" },
+                    lualine_y = { "progress" },
+                    lualine_z = { "location" },
+                },
+            })
+        end,
+    }
+}
